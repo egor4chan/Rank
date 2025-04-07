6cce3eb076164b0aeeea2b0b3b5aa4ad0f20f405
egor4chan <93943877+egor4chan@users.noreply.github.com>
2025-04-07 21:23:26 +0400

    Update Download.js

--- static/script/Download.js
+++ static/script/Download.js
@@ -10,7 +10,9 @@ function download(file_name) {
 
 gifts = ['pepe_0', 'pepe_1', 'pepe_2', 'pepe_3', 'pepe_4', 'pepe_5', 'pepe_6', 'pepe_7', 'cap_8', 'cap_9', 'cap_10', 'cap_11', 'cap_12', 'cap_13', 'peach_14', 'peach_15', 'peach_16', 'peach_17']
 
-for (let i=0; i < gifts.length; i++) {
-    let id = gifts[i]
-    download(id)
-}
+//for (let i=0; i < gifts.length; i++) {
+//    let id = gifts[i]
+//    download(id)
+//}
+
+download('pepe_0')
