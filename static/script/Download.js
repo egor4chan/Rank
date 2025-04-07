2d1170e4b393a2a7d8d90369e1a55907fcb12dbc
egor4chan <93943877+egor4chan@users.noreply.github.com>
2025-04-07 21:48:01 +0400

    Update Download.js

--- static/script/Download.js
+++ static/script/Download.js
@@ -1,10 +1,5 @@
 function download(file_name) {
-    var gif = document.createElement('img')
-    gif.setAttribute('src', `static/images/gifs/${file_name}.gif`)
-
-    
     document.getElementById(`${file_name}`).setAttribute('src', `static/images/gifs-low/${file_name}-min.gif`)
-    
 }
 
 gifts = ['pepe_0', 'pepe_1', 'pepe_2', 'pepe_3', 'pepe_4', 'pepe_5', 'pepe_6', 'pepe_7', 'cap_8', 'cap_9', 'cap_10', 'cap_11', 'cap_12', 'cap_13', 'peach_14', 'peach_15', 'peach_16', 'peach_17']
