80c12651111f107cb23a25ab1a0b4b8ba523cf6d
egor4chan <93943877+egor4chan@users.noreply.github.com>
2025-04-07 21:40:25 +0400

    as

--- static/script/Download.js
+++ static/script/Download.js
@@ -3,16 +3,13 @@ function download(file_name) {
     gif.setAttribute('src', `static/images/gifs/${file_name}.gif`)
 
     gif.onload = function() {
-        document.getElementById(`${file_name}`).setAttribute('src', `static/images/gifs/${file_name}.gif`)
-        alert(file_name)
+        document.getElementById(`${file_name}`).setAttribute('src', `static/images/gifs-low/${file_name}-min.gif`)
     }
 }
 
 gifts = ['pepe_0', 'pepe_1', 'pepe_2', 'pepe_3', 'pepe_4', 'pepe_5', 'pepe_6', 'pepe_7', 'cap_8', 'cap_9', 'cap_10', 'cap_11', 'cap_12', 'cap_13', 'peach_14', 'peach_15', 'peach_16', 'peach_17']
 
-//for (let i=0; i < gifts.length; i++) {
-//    let id = gifts[i]
-//    download(id)
-//}
-
-download('pepe_0')
+for (let i=0; i < gifts.length; i++) {
+    let id = gifts[i]
+    download(id)
+}
